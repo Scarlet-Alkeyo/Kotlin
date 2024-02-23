fde59741cda103ac50829ce4b62dd7dfa21309cd
Scarlet Alkeyo <scarletalkeyo@gmail.com>
2024-02-23 08:17:55 +0300

    added files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,37 @@
+fun main(){
+    modulus(3,4)
+    println("Hello Scarlet")
+    sum(3,5,4,3)
+    println("singing")
+}
+fun printHello(name: String){
+    println(name)
+}
+
+fun modulus(num1:Int, num2:Int,):Int{
+    var result = num1 % num2
+    return result
+}
+fun sum(num1 :Int,num2 :Int,num3:Int,num4 :Int):Int{
+    var result = num1 + num2 +num2 +num3 +num4
+    return result
+}
+
+fun funnyFact(word : String){
+    println(word)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
